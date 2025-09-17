477169ee91ffc5206ce73ccc1d5fe4d45c009b10
AziendaAgricolaBesostri <centralebesostri@hotmail.com>
2025-09-17 14:39:02 +0200

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,25 +1,18 @@
-// Fill these with your Firebase Web app config (Console Firebase > Impostazioni progetto > App Web)
+// lib/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      return const firebaseConfig = {
-        apiKey: "AIzaSyBeaK3IVK2u9Zj9_UAxrZbTGaLy-IMbxAE",
-        authDomain: "besostri-farm.firebaseapp.com",
-        projectId: "besostri-farm",
-        storageBucket: "besostri-farm.firebasestorage.app",
-        messagingSenderId: "305249453373",
-        appId: "1:305249453373:web:d2453c53daf8c61dadb154",
-        measurementId: "G-4436TL3S98"
-      };
-    }
+    // Usiamo sempre la config Web
     return const FirebaseOptions(
-      apiKey: 'PASTE_API_KEY',
-      appId: 'PASTE_APP_ID',
-      messagingSenderId: 'PASTE_SENDER_ID',
-      projectId: 'PASTE_PROJECT_ID',
+      apiKey: 'INSERISCI_API_KEY', // es. AIzaSyB...
+      appId: 'INSERISCI_APP_ID', // es. 1:1234567890:web:abcd1234efgh5678
+      messagingSenderId: 'INSERISCI_SENDER_ID', // es. 1234567890
+      projectId: 'INSERISCI_PROJECT_ID', // es. besostri-farm
+      authDomain: 'INSERISCI_AUTH_DOMAIN', // es. besostri-farm.firebaseapp.com
+      storageBucket: 'INSERISCI_STORAGE_BUCKET', // es. besostri-farm.appspot.com
+      measurementId: 'INSERISCI_MEASUREMENT_ID', // es. G-XXXXXXX (se non c'è, lascia '' )
     );
   }
 }
