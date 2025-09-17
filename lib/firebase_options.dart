61a700751a703145806a442c901704ec3bf7874d
AziendaAgricolaBesostri <centralebesostri@hotmail.com>
2025-09-17 09:30:25 +0200

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,15 +5,15 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return const FirebaseOptions(
-        apiKey: 'PASTE_API_KEY',
-        appId: 'PASTE_APP_ID',
-        messagingSenderId: 'PASTE_SENDER_ID',
-        projectId: 'PASTE_PROJECT_ID',
-        authDomain: 'PASTE_PROJECT_ID.firebaseapp.com',
-        storageBucket: 'PASTE_PROJECT_ID.appspot.com',
-        measurementId: 'PASTE_MEASUREMENT_ID',
-      );
+      return const firebaseConfig = {
+        apiKey: "AIzaSyBeaK3IVK2u9Zj9_UAxrZbTGaLy-IMbxAE",
+        authDomain: "besostri-farm.firebaseapp.com",
+        projectId: "besostri-farm",
+        storageBucket: "besostri-farm.firebasestorage.app",
+        messagingSenderId: "305249453373",
+        appId: "1:305249453373:web:d2453c53daf8c61dadb154",
+        measurementId: "G-4436TL3S98"
+      };
     }
     return const FirebaseOptions(
       apiKey: 'PASTE_API_KEY',
